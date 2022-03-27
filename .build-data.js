97def6472084bbd26ac9f4b2d53d8496ed576728
Pranic Lift <praniclift@gmail.com>
2022-03-26 22:31:50 -0500

    Add localization support

--- .build-data.js
+++ .build-data.js
@@ -1,12 +1,28 @@
 var _ = require('lodash');
 var fs = require('fs');
+var langs = [
+    'en', // English
+    'de', // German
+    'fr', // French
+    'it', // Italian
+    'ko', // Korean
+    'es', // Spanish
+    'zh-cn', // Simplified Chinese
+    'zh-tw', // Traditional Chinese
+    'ru', // Russian
+    'th', // Thai
+    'jp', // Japanese
+    'pl', // Polish
+    'pt', // Portuguese
+    'es-419', // Spanish - Latin America
+];
 
 function compressGameDataForDelivery() {
     var data = JSON.parse(fs.readFileSync(__dirname + '/assets/json/gamedata.json'));
     var fields;
 
-    for(var key in data.weapons) {
-        if(! fields) {
+    for (var key in data.weapons) {
+        if (!fields) {
             fields = _.keys(data.weapons[key]);
         }
 
@@ -18,8 +34,32 @@ function compressGameDataForDelivery() {
         'data': data.weapons,
     };
 
-    // Move the new file into the public folder
+    // Put the final data file in the public folder
     fs.writeFileSync('./_site/json/gamedata.json', JSON.stringify(data));
 }
 
+
+function makeLanguageFile(lang) {
+    var gamedata = JSON.parse(fs.readFileSync(__dirname + '/assets/json/gamedata.json'));
+    var data = JSON.parse(fs.readFileSync(__dirname + '/assets/json/lang_' + lang + '.json'));
+
+    var r = {
+        weapons: _.mapValues(_.invert(Object.keys(gamedata.weapons)), (value, key) => {
+            return {
+                'name': data['weaponNames'][key],
+                'description': data['weaponCaptions'][key]
+            };
+        })
+    };
+
+    // Put the new file in the public folder
+    fs.writeFileSync('./_site/json/lang_' + lang + '.json', JSON.stringify(r));
+}
+
+
+// Compress the large JSON file into smol JSON file
 compressGameDataForDelivery();
+
+// Re-make all of the necessary language files, stripping out duplicated content
+langs.forEach(makeLanguageFile);
+
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,29 @@
 import Alpine from 'alpinejs'
+import AlpineI18n from 'alpinejs-i18n'
 Window._ = require('lodash')
 
 window.Alpine = Alpine
 
+Alpine.plugin(AlpineI18n)
+
 Alpine.data('app', () => ({
+    defaultLang: 'en',
+    availableLangs: [
+        'en', // English
+        'de', // German
+        'fr', // French
+        'it', // Italian
+        'ko', // Korean
+        'es', // Spanish
+        'zh-cn', // Simplified Chinese
+        'zh-tw', // Traditional Chinese
+        'ru', // Russian
+        'th', // Thai
+        'jp', // Japanese
+        'pl', // Polish
+        'pt', // Portuguese
+        'es-419', // Spanish - Latin America
+    ],
     loading: true,
     gamedata: null,
     currentList: function () {
@@ -39,7 +59,54 @@ Alpine.data('app', () => ({
         'title': 'Elden Ring Quick Lookup',
         'search_label': 'Search',
     },
+    loadLanguage(lang) {
+        fetch('/json/lang_' + lang + '.json')
+            .then(res => {
+                return res.status == 200 ? res.json() : null;
+            })
+            .then(data => {
+                var newData = {};
+                newData[lang] = data;
+
+                window.AlpineI18n.create(lang, newData);
+                // AlpineI18n.locale = lang;
+                // AlpineI18n.fallbackLocale = 'en';
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    },
+    getClosestAvailableLanguage(lang) {
+        lang = lang.toLowerCase().trim();
+
+        if (this.availableLangs.includes(lang)) {
+            // Exists as-is in the available langs.
+            return lang;
+        }
+
+        for(var i = 0; i < this.availableLangs.length; i++) {
+            var knownLang = this.availableLangs[i];
+
+            if (lang.indexOf(knownLang) === 0) {
+                // Found this language, but not the specific region/dialect
+                return this.availableLangs[i];
+            }
+        }
+    },
     init() {
+        this.loadLanguage(this.defaultLang);
+        AlpineI18n.fallbackLocale = 'en';
+        
+        var userLang = this.getClosestAvailableLanguage(window.navigator.userLanguage || window.navigator.language);
+        // userLang = 'en';
+        // console.log('user lang: ' + userLang);
+        
+        if (this.defaultLang != userLang) {
+            this.loadLanguage(userLang);
+        }
+        
+        AlpineI18n.locale = userLang;
+
         fetch('/json/gamedata.json')
             .then(res => res.json())
             .then(data => {
